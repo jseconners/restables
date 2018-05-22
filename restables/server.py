3028147b63ab1a65934195473f64f0636da9dbf6
James Conners <jseconners@gmail.com>
2018-05-22 15:51:42 -0700

    Added mysql-connector and stubbing out some rough/unsafe basic functionality

--- restables/server.py
+++ restables/server.py
@@ -1,7 +1,8 @@
 import os
 import json
 import yaml
-from flask import Flask, abort, request
+from flask import Flask, abort, request, Response, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -16,6 +17,49 @@ with open(config_file, 'r') as cfg_file:
 def welcome():
     return "<h1>Welcome to restables!</h1>"
 
-@app.route('/<table>', methods=['GET'])
-def info(table):
-    return table
+
+@app.route('/<connection>', methods=['GET'])
+def db_info(connection):
+
+    if connection in config['databases']:
+        cfg = config['databases'][connection]
+        cnx = mysql.connector.connect(**cfg)
+
+    cursor = cnx.cursor()
+    query = ("SHOW TABLES FROM `{}`".format(cfg['database']))
+    cursor.execute(query)
+
+    res = []
+    for (table, ) in cursor:
+        res.append(table)
+
+    return jsonify(res)
+
+@app.route('/<connection>/<table>', methods=['GET'])
+def table_info(connection, table):
+    if connection in config['databases']:
+        cfg = config['databases'][connection]
+        cnx = mysql.connector.connect(**cfg)
+
+    # get all the tables from the database
+    cursor = cnx.cursor()
+    query = ("SHOW TABLES FROM `{}`".format(cfg['database']))
+    cursor.execute(query)
+
+    tables = []
+    for (t, ) in cursor:
+        tables.append(t)
+
+    # Table name is safe if the passed table name is valid, i.e. in the table
+    # list from the db, then use it in the query.
+    if (table in tables):
+        query = ("SELECT * FROM `{}`.`{}`".format(cfg['database'], table))
+        cursor.execute(query)
+    else:
+        abort(404)
+
+    res = []
+    for row in cursor:
+        res.append(row)
+
+    return jsonify(res)
