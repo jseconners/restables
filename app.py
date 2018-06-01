74e361f1e8caf691dc250bcf0dad16a7e7111ae8
James Conners <jseconners@gmail.com>
2018-06-01 14:40:47 -0700

    Loading dbs.yaml from instance/ dir. Added dbs.yaml.example to repo

--- app.py
+++ app.py
@@ -1,33 +1,28 @@
-import os
 import yaml
 import utils
 from flask import Flask, jsonify, g, stream_with_context, Response
 
 app = Flask(__name__)
 
-# database connection configs
-CONFIG_FILE = os.path.join(app.root_path, 'config.yaml')
-
 
 @app.before_request
-def load_config():
-    """ Load app configuration and store in app context """
-    g._config = None
-    with open(CONFIG_FILE, 'r') as cfg_file:
-        g._config = yaml.load(cfg_file)
+def load_dbconfigs():
+    """ Load database configurations """
+    with app.open_instance_resource('dbs.yaml') as f:
+        g._dbconfigs = yaml.load(f)
 
 
 @app.route('/')
 def db_list():
     """ Return a list of available configured connections """
-    available_connections = g._config['databases'].keys()
+    available_connections = g._dbconfigs.keys()
     return jsonify(list(available_connections))
 
 
 @app.route('/<connection_name>', methods=['GET'])
 def db_info(connection_name):
     """ Return a list of tables for this connection/database """
-    db = utils.DBCon(g._config['databases'][connection_name])
+    db = utils.DBCon(g._dbconfigs[connection_name])
     info = {
         'tables': db.get_tables()
     }
@@ -37,7 +32,7 @@ def db_info(connection_name):
 @app.route('/<connection_name>/<table>', methods=['GET'])
 def table_info(connection_name, table):
     """ Return table info, i.e. columns and row count """
-    db = utils.DBCon(g._config['databases'][connection_name])
+    db = utils.DBCon(g._dbconfigs[connection_name])
     info = {
         'columns': db.get_column_names(table),
         'rows': db.get_table_count(table)
@@ -52,7 +47,7 @@ def table_data(connection_name, table, fields, opts):
     Return plain/text CSV for table data, with specified field list and
     options list
     """
-    db = utils.DBCon(g._config['databases'][connection_name])
+    db = utils.DBCon(g._dbconfigs[connection_name])
 
     results = db.get_table_data(table, fields, opts)
 
